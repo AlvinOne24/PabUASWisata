acf05f4a883ed60ca18b641cd9fd4e1b66e56e90
AlvinOne <al24vin@mhs.mdp.ac.id>
2023-06-15 18:45:37 +0700

    Ubah dan Delete

--- app/src/main/java/com/si61/wisataalvin/Activity/UbahActivity.java
+++ app/src/main/java/com/si61/wisataalvin/Activity/UbahActivity.java
@@ -2,15 +2,90 @@ package com.si61.wisataalvin.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.si61.wisataalvin.API.APIRequestData;
+import com.si61.wisataalvin.API.RetroServer;
+import com.si61.wisataalvin.Model.ModelResponse;
 import com.si61.wisataalvin.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UbahActivity extends AppCompatActivity {
+    private EditText etNama, etLokasi, etUrlmap;
+    private Button btnSimpan;
+    private String  nama, lokasi, urlmap;
+    private  String yId, yNama, yLokasi, yUrlmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubah);
+
+        etNama = findViewById(R.id.et_nama);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etUrlmap = findViewById(R.id.et_urlmap);
+        btnSimpan = findViewById(R.id.btn_simpan);
+
+        Intent tangkap = getIntent();
+        yId = tangkap.getStringExtra("xId");
+        yNama = tangkap.getStringExtra("xNama");
+        yLokasi = tangkap.getStringExtra("xLokasi");
+        yUrlmap = tangkap.getStringExtra("xUrlmap");
+
+        etNama.setText(yNama);
+        etLokasi.setText(yLokasi);
+        etUrlmap.setText(yUrlmap);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nama = etNama.getText().toString();
+                lokasi = etLokasi.getText().toString();
+                urlmap = etUrlmap.getText().toString();
+
+                if(nama.trim().isEmpty()){
+                    etNama.setError("Nama Tidak Boleh Kosong");
+                }
+                else if(lokasi.trim().isEmpty()){
+                    etLokasi.setError("Lokasi Tidak Boleh Kosong");
+                }
+                else if(urlmap.trim().isEmpty()){
+                    etUrlmap.setError("UrlMap Tidak Boleh Kosong");
+                }
+                else{
+                    prossesUbah();
+
+                }
+            }
+        });
+    }
+
+    private void  prossesUbah(){
+        APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = API.ardUpdate(yId, nama, lokasi, urlmap);
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(UbahActivity.this, "Kode : " + kode + "Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(UbahActivity.this, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/si61/wisataalvin/Adapter/AdapterWisata.java
+++ app/src/main/java/com/si61/wisataalvin/Adapter/AdapterWisata.java
@@ -1,18 +1,32 @@
 package com.si61.wisataalvin.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.si61.wisataalvin.API.APIRequestData;
+import com.si61.wisataalvin.API.RetroServer;
+import com.si61.wisataalvin.Activity.MainActivity;
+import com.si61.wisataalvin.Activity.TambahActivity;
+import com.si61.wisataalvin.Activity.UbahActivity;
+import com.si61.wisataalvin.Model.ModelResponse;
 import com.si61.wisataalvin.Model.ModelWisata;
 import com.si61.wisataalvin.R;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterWisata extends RecyclerView.Adapter<AdapterWisata.VHWisata> {
     private Context ctx;
@@ -56,6 +70,58 @@ public class AdapterWisata extends RecyclerView.Adapter<AdapterWisata.VHWisata> 
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvLokasi = itemView.findViewById(R.id.tv_lokasi);
             tvUrlMap = itemView.findViewById(R.id.tv_urlmap);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Anda Memilih "+tvNama.getText().toString()+"Operasi apa yang akan dilakukan");
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent kirim = new Intent(ctx, UbahActivity.class);
+                            kirim.putExtra("xId", tvId.getText().toString());
+                            kirim.putExtra("xNama", tvNama.getText().toString());
+                            kirim.putExtra("xLokasi", tvLokasi.getText().toString());
+                            kirim.putExtra("xUrlmap", tvUrlMap.getText().toString());
+                            ctx.startActivity(kirim);
+                        }
+                    });
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            prosesHapus(tvId.getText().toString());
+
+                        }
+                    });
+
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+        void prosesHapus(String id){
+            APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = API.arddelete(id);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode : " + kode + "Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveWisata();
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx, "Gagal menghubungi Server: "+ t.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
         }
     }
 }
